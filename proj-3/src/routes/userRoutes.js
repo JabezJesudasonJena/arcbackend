c76ec0eac79b77e5a67293abcfba62cfff664bcd
JabezJesudasonJena <boycalledjabez@gmail.com>
2026-04-02 07:46:19 +0530

    Completed Role based access Auth
    Added three routes GET /role/* the * representes three different endpooints having role based access admin for admin, manager for manager and user for all users

--- proj-3/src/routes/userRoutes.js
+++ proj-3/src/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { verifyToken } from "../middlewares/authMiddleware.js";
+import { authorizeRoles } from "../middlewares/roleMiddleware.js";
 
 const userRoute = express.Router();
 
-userRoute.get('/admin',verifyToken, (req, res) => {
+userRoute.get('/admin',verifyToken, authorizeRoles("admin"),(req, res) => {
     res.json({message: "Welcome Admin !"})
 })
 
-userRoute.get('/manager', (req, res) => {
+userRoute.get('/manager', verifyToken, authorizeRoles("manager"), (req, res) => {
     res.json({message: "Welcome Manager !"})
 })
 
